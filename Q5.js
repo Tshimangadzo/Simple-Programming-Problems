27e07a92012559b477422fc6aa008499caae4f34
Tshimangadzo <mudzanani27@gmail.com>
2021-11-20 01:43:56 +0200

    modified fucntion to sum for multiplies of 3 and 5

--- Q5.js
+++ Q5.js
@@ -1,9 +1,9 @@
 let sumNumbers =(maxNum)=>{
     let sum = 0;
     for(let i = 1; i<= maxNum;i++){
-        if(i%3 === 0 || i%2 === 0) sum +=i;
+        if(i%3 === 0 || i%5 === 0) sum +=i;
     }
  
   return sum
 }
-console.log(sumNumbers(5))
+console.log(sumNumbers(17))
